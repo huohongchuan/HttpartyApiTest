f639b668a0096a10912744dbfeb22e134695f3a2
huohongchuan <huohongchuan@126.com>
2016-08-24 15:28:16 +0800

    新增set_response111

--- features/steps/InterfaceTestDemo.rb
+++ features/steps/InterfaceTestDemo.rb
@@ -1,48 +1,61 @@
 # encoding: utf-8
-require 'httparty'
+#require 'httparty'
 require 'json'
-require 'jsonpath'
+
+require File.dirname(__FILE__)+'/set_request_parameter'
+require File.dirname(__FILE__)+'/set_request'
+require File.dirname(__FILE__)+'/set_response'
 
 class InterfaceTestDemo
-  include HTTParty
-  #attr_accessor :options
+    include SetRequestParameter
+    include SetRequest
+    include SetResponse
 
-  def headers=(hd)
-  	header = JSON.parse hd
-  	@headers = ｛headers:header}
-  end
+
+    def initialize
+      @options = {}
+      @url = ''
+    end
+
+
+    def out
+      puts @options
+      puts @options.class
+    end
 
 
 end
 
-require 'json'
-class TestAbc
-	def initialize
-		@hs = {}
-	end
-	def test1(h={})
-		@hs[:header] = {}
-		@hs[:header] = h	
-	end
-
-	def test2(b={})
-		@hs[:body] = {}
-		@hs[:body] = b
-	end
-	def out
-		puts @hs
-		puts @hs.class
-	end
-end
-#@hs = Hash.new
 str = %Q({"Accept":"text/html", "xxxx":"bbb"})
-str1 = '{"login": "email@example.com", "password": "password"}'
-str4 = '{"loginxxxxxxxx": "email@example.com", "password": "password"}'
-str2 = JSON.parse(str)
-str3 = JSON.parse(str1)
-str5 = JSON.parse(str4)
+str1 = '{"wx_appid": "wx1b023b900d9317ef", "wx_openid": "oeGgrwBb8saWkLQyzvl15z0A_Bdo"}'
+str4 = {:wx_appid => 'wx1b023b900d9317ef', :wx_openid => 'oeGgrwBb8saWkLQyzvl15z0A_Bdo'}
 
-testabc = TestAbc.new
-testabc.test1(str2)
-testabc.test2(str3)
-testabc.out
+testabc = InterfaceTestDemo.new
+#testabc.setheader(str)
+#testabc.set_query(str4)
+#testabc.get_file("/home/hhc/work/rubytest/hhc.json")
+testabc.get_value(str4)
+testabc.set_query
+
+testabc.set_options
+testabc.options
+testabc.set_method('GET', 'http://system.cpd.test.social-touch.com/Api/Kefu/dialogStatus?')
+testabc.get_response
+#puts testabc.response.headers.inspect
+puts testabc.response_header.class
+testabc.headersh
+#puts testabc.codesh
+#ptus testabc.bodysh
+
+#respone = HTTParty.get('http://system.cpd.test.social-touch.com/Api/Kefu/dialogStatus?', aaa)
+#respone = HTTParty.get('http://www.baidu.com')
+#ccc = respone.headers.inspect
+#puts ccc
+#puts ccc.class
+#bbb = JSON.parse(respone.body)
+#ddd = JSON.parse(ccc.gsub!(/=>/, ':'))
+#puts ddd.class
+#puts ddd
+#puts respone.body
+#puts bbb
+#puts bbb.class
