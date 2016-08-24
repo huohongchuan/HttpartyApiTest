97d42f16d29b7185f68396f1c2364c8bfa93f21f
huohongchuan <huohongchuan@126.com>
2016-08-24 15:28:37 +0800

    新增set_response111

--- features/steps/set_response.rb
+++ features/steps/set_response.rb
@@ -4,26 +4,34 @@ require 'jsonpath'
 require 'httparty'
 
 module SetResponse
-  attr_accessor :response
+  attr_accessor :response, :response_header, :response_body, :response_code, :response_message
 
-#
-  def response_hash
-    JSON.parser(self)
+#批量替换字符串中"=>"为":"
+  def make_to_hash(res)
+    bbb = res.gsub(/=>/, ':')
+    JSON.parser(bbb)
   end
 
-  def get_header
-    response.headers.inspect
+  def get_response
+    @response_header= response.headers.inspect
+    @response_body= response.body
+    @response_code= response.code
+    @response_message= response.message
   end
 
-  def get_body
-    response.body
+  def headersh
+    make_to_hash @response_header
   end
 
-  def get_code
-    response.code
+  def bodysh
+    make_to_hash @response_body
   end
 
-  def get_message
-    response.message
+  def codesh
+    make_to_hash @response_code
+  end
+
+  def messagesh
+    make_to_hash @response_message
   end
 end
